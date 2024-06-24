cc36fa2e186c2270e54c66f8bc0f9bf4c1dcf934
anbukannadhasan <154507930+Anbukannadhasan@users.noreply.github.com>
2024-06-24 10:00:37 +0100

    gh #11 update def grp in L2 files

--- src/test_l2_dsDisplay.c
+++ src/test_l2_dsDisplay.c
@@ -17,23 +17,62 @@
 */
 
 /**
-* @file test_l2_dsDisplay.c
-* @page dsDisplay Level 2 Tests
-*
-* ## Module's Role
-* This module includes Level 2 functional tests (success and failure scenarios).
-* This is to ensure that the dsDisplay APIs meet the requirements across all vendors.
-*
-* **Pre-Conditions:**  None@n
-* **Dependencies:** None@n
-*
-* Ref to API Definition specification documentation : [ds-display_halSpec.md](../../docs/pages/ds-display_halSpec.md)
-*/
+ * @addtogroup HPK Hardware Porting Kit
+ * @{
+ * @par The Hardware Porting Kit
+ * HPK is the next evolution of the well-defined Hardware Abstraction Layer
+ * (HAL), but augmented with more comprehensive documentation and test suites
+ * that OEM or SOC vendors can use to self-certify their ports before taking
+ * them to RDKM for validation or to an operator for final integration and
+ * deployment. The Hardware Porting Kit effectively enables an OEM and/or SOC
+ * vendor to self-certify their own Video Accelerator devices, with minimal RDKM
+ * assistance.
+ *
+ */
+
+/**
+ * @addtogroup Device_Settings Device Settings Module
+ * @{
+ */
+
+/**
+ * @addtogroup Device_Settings_HALTEST Device Settings HAL Tests
+ * @{
+ */
+
+/**
+ * @defgroup DS_Display_HALTEST Device Settings Display HAL Tests
+ * @{
+ */
+
+/**
+ * @defgroup DS_Display_HALTEST_L2 Device Settings Display HAL Tests L2 File
+ * @{
+ * @parblock
+ *
+ * ### L2 Tests for DS Display HAL :
+ *
+ * Level 2 unit test cases for all APIs of Device Settings Display HAL
+ *
+ * **Pre-Conditions:**  None@n
+ * **Dependencies:** None@n
+ *
+ * Refer to API Definition specification documentation : [ds-display_halSpec.md](../../docs/pages/ds-display_halSpec.md)
+ *
+ * @endparblock
+ */
+
+
+/**
+ * @file test_l2_dsDisplay.c
+ *
+ */
 
 #include <ut.h>
 #include <ut_log.h>
 #include <ut_kvp_profile.h>
 #include "dsDisplay.h"
+#include "test_parse_configuration.h"
 
 static int gTestGroup = 2;
 static int gTestID = 1;
@@ -46,7 +85,7 @@ static int gTestID = 1;
 * **Test Case ID:** 001@n
 *
 * **Test Procedure:**
-* Refer to UT specification documentation [dsDisplay_L2_Low-Level_TestSpecification.md](../../docs/pages/dsDisplay_L2_Low-Level_TestSpecification.md)
+* Refer to UT specification documentation [dsDisplay_L2_Low-Level_TestSpecification.md](../docs/pages/ds-display-L2-Low-Level_TestSpec.md)
 */
 
 void test_l2_dsDisplay_RetrieveAndValidateEDID_sink(void)
@@ -59,6 +98,8 @@ void test_l2_dsDisplay_RetrieveAndValidateEDID_sink(void)
     dsDisplayEDID_t edid = { 0 }; // Initialize all edid fields to 0.
     unsigned char edidBytes[MAX_EDID_BYTES_LEN] = { 0 }; // Initialize the edidbytes to 0.
     int length = 0; // Initialize the length to 0.
+    unsigned char edid_profile;
+    char key_string[TEST_DS_KEY_SIZE];
 
     // Step 1: Call dsDisplayInit
     ret = dsDisplayInit();
@@ -80,14 +121,8 @@ void test_l2_dsDisplay_RetrieveAndValidateEDID_sink(void)
     ret = dsGetEDID(handle, &edid);
     UT_ASSERT_EQUAL(ret, dsERR_NONE);
     UT_LOG_INFO("Invoked dsGetEDID() with handle %ld, returned: %d, productCode: %d\n", handle, ret, edid.productCode);
-    if (ret != dsERR_NONE)
-    {
-        // Call dsDisplayTerm if dsGetEDID fails
-        dsDisplayTerm();
-        return;
-    }
 
-    UT_ASSERT_KVP_EQUAL_PROFILE_UINT32(edid.productCode, "EDID_Data/productCode");
+    UT_ASSERT_KVP_EQUAL_PROFILE_UINT32(edid.productCode, "dsDisplay/EDID_Data/productCode");
 
     // Step 4: Call dsGetEDIDBytes
     ret = dsGetEDIDBytes(handle, edidBytes, &length);
@@ -101,10 +136,17 @@ void test_l2_dsDisplay_RetrieveAndValidateEDID_sink(void)
     }
 
     // Manufacturer ID
-    UT_LOG_INFO("Manufacturer ID: %d\n", edidBytes[8] << 8 | edidBytes[9]);
-
-    //TODOs : Need KVP support for array handling
-    UT_ASSERT_KVP_EQUAL_PROFILE_UINT16(edidBytes[8] << 8 | edidBytes[9], "EDID_Data/edidBytes");
+    for( uint8_t i = 8; i < 9; i++)
+    {
+        snprintf(key_string, MAX_EDID_BYTES_LEN, "dsDisplay.edidBytes.%d", i);
+        edid_profile = UT_KVP_PROFILE_GET_UINT8(key_string);
+        if(edid_profile != edidBytes[i])
+        {
+            UT_FAIL("edid check failed");
+            UT_LOG_ERROR("edid byte: %x, expected value: %x", edidBytes[i], edid_profile);
+            break;
+        }
+    }
 
     // Step 5: Call dsDisplayTerm
     ret = dsDisplayTerm();
@@ -123,7 +165,7 @@ void test_l2_dsDisplay_RetrieveAndValidateEDID_sink(void)
 * **Test Case ID:** 002@n
 *
 * **Test Procedure:**
-* Refer to UT specification documentation [dsDisplay_L2_Low-Level_TestSpecification.md](../../docs/pages/dsDisplay_L2_Low-Level_TestSpecification.md)
+* Refer to UT specification documentation [dsDisplay_L2_Low-Level_TestSpecification.md](../docs/pages/ds-display-L2-Low-Level_TestSpec.md)
 */
 
 void test_l2_dsDisplay_TestDefaultAspectRatio_source(void)
@@ -159,8 +201,6 @@ void test_l2_dsDisplay_TestDefaultAspectRatio_source(void)
     if (ret != dsERR_NONE || aspectRatio != dsVIDEO_ASPECT_RATIO_16x9)
     {
         UT_LOG_ERROR("dsGetDisplayAspectRatio() failed with error: %d\n", ret);
-        dsDisplayTerm();
-        return;
     }
 
     // Step 4: Verify aspect ratio
@@ -176,8 +216,6 @@ void test_l2_dsDisplay_TestDefaultAspectRatio_source(void)
 }
 
 static UT_test_suite_t * pSuite = NULL;
-static UT_test_suite_t * pSuite1 = NULL;
-
 /**
  * @brief Register the main tests for this module
  *
@@ -186,23 +224,51 @@ static UT_test_suite_t * pSuite1 = NULL;
 
 int test_l2_dsDisplay_register(void)
 {
+     int32_t source_type = 0;
+     ut_kvp_status_t status;
+
+    status = ut_kvp_getStringField(ut_kvp_profile_getInstance(), "dsDisplay.Type", gDeviceType, TEST_DS_DEVICE_TYPE_SIZE);
+
     // Create the test suite
-    pSuite = UT_add_suite("[L2 dsDisplay Sink]", NULL, NULL);
-    if (pSuite == NULL)
-    {
+    if (status == UT_KVP_STATUS_SUCCESS ) {
+        if (!strncmp(gDeviceType, TEST_TYPE_SOURCE_VALUE, TEST_DS_DEVICE_TYPE_SIZE)) {
+            pSuite = UT_add_suite("[L2 dsDisplay Sink]", NULL, NULL);
+            if (pSuite == NULL)
+            {
+                return -1;
+            }
+            source_type = 1;
+        }
+        else if(!strncmp(gDeviceType, TEST_TYPE_SINK_VALUE, TEST_DS_DEVICE_TYPE_SIZE)) {
+            pSuite = UT_add_suite("[L2 dsDisplay Source ]", NULL, NULL);
+            if (pSuite == NULL)
+            {
+                return -1;
+            }
+            source_type = 0;
+        }
+        else {
+            UT_LOG_ERROR("Invalid platform type: %s", gDeviceType);
+            return -1;
+        }
+    }
+    else {
+        UT_LOG_ERROR("Failed to get the platform type");
         return -1;
     }
-
-    pSuite1 = UT_add_suite("[L2 dsDisplay Source ]", NULL, NULL);
-    if (pSuite == NULL)
-    {
-        return -1;
-    }
-
     // List of test function names and strings
-
-    UT_add_test( pSuite, "l2_dsDisplay_RetrieveAndValidateEDID_sink", test_l2_dsDisplay_RetrieveAndValidateEDID_sink);
-    UT_add_test( pSuite1, "l2_dsDisplay_TestDefaultAspectRatio_source", test_l2_dsDisplay_TestDefaultAspectRatio_source);
+    if(source_type == 0) {
+        UT_add_test( pSuite, "L2_RetrieveAndValidateEDID_sink", test_l2_dsDisplay_RetrieveAndValidateEDID_sink);
+    }
+    else if ( source_type == 1 ){
+        UT_add_test( pSuite, "L2_TestDefaultAspectRatio_source", test_l2_dsDisplay_TestDefaultAspectRatio_source);
+    }
 
     return 0;
 }
+
+/** @} */ // End of DS_Display_HALTEST_L2
+/** @} */ // End of DS_Display_HALTEST
+/** @} */ // End of Device_Settings_HALTEST
+/** @} */ // End of Device_Settings
+/** @} */ // End of HPK
