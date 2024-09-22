e9be2e75168117aad5804d8bfa3f4020ebe8f80f
Ulrond <131959864+Ulrond@users.noreply.github.com>
2024-09-22 20:33:06 +0100

    upgrade #159 corrected install requirements

--- host/install.sh
+++ host/install.sh
@@ -167,7 +167,7 @@ install_pip_requirements "${RAFT_DIR}"/requirements.txt
 
 # Setup ut-raft
 clone_repo git@github.com:rdkcentral/ut-raft.git "${PLUGINS_FRAMEWORK_DIR}/ut_raft" develop "in ./raft/framework/plugins"
-install_pip_requirements "${TESTS_DIR}"/ut_raft/requirements.txt
+install_pip_requirements "${PLUGINS_FRAMEWORK_DIR}"/ut_raft/requirements.txt
 
 ## Install your own sub git repo's in here as required
 # 
