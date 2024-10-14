d5ce784e7e436b87cd428a92ed8b5242a90ebbb6
ishthiyaqahmed <167058902+ishthiyaqahmed@users.noreply.github.com>
2024-10-14 06:01:08 +0100

    gh #202 HdmiIn python L3 test cases review comments addressal.

--- host/tests/dsHdmiIn_L3_Tests/dsHdmiIn_test4_VideoModeChangeCallback_Verify.py
+++ host/tests/dsHdmiIn_L3_Tests/dsHdmiIn_test4_VideoModeChangeCallback_Verify.py
@@ -34,40 +34,6 @@ from raft.framework.plugins.ut_raft.configRead import ConfigRead
 from raft.framework.plugins.ut_raft.utPlayer import utPlayer
 from raft.framework.plugins.ut_raft.utUserResponse import utUserResponse
 
-class hdmiVideoResolution(Enum):
-      dsVIDEO_PIXELRES_720x480   = 0     #720x480 Resolution
-      dsVIDEO_PIXELRES_720x576   = 1     #720x576 Resolution
-      dsVIDEO_PIXELRES_1280x720  = 2     #1280x720 Resolution
-      dsVIDEO_PIXELRES_1366x768  = 3     #1366x768 Resolution
-      dsVIDEO_PIXELRES_1920x1080 = 4     #1920x1080 Resolution
-      dsVIDEO_PIXELRES_3840x2160 = 5     #3840x2160 Resolution
-      dsVIDEO_PIXELRES_4096x2160 = 6     #3840x2160 Resolution
-      dsVIDEO_PIXELRES_MAX = 7           #dsVIDEO_PIXELRES_MAX
-
-class hdmiVideoAspectRatio(Enum):
-      dsVIDEO_ASPECT_RATIO_4x3 = 0    #< 4:3 aspect ratio
-      dsVIDEO_ASPECT_RATIO_16x9 = 1   #< 16:9 aspect ratio
-      dsVIDEO_ASPECT_RATIO_MAX = 2    #< Out of range
-
-class hdmiVideoStereoScopicMode(Enum):
-    dsVIDEO_SSMODE_UNKNOWN = 0            #Unknown mode
-    dsVIDEO_SSMODE_2D, = 1                #2D mode
-    dsVIDEO_SSMODE_3D_SIDE_BY_SIDE = 2    #3D side by side (L/R) stereo mode
-    dsVIDEO_SSMODE_3D_TOP_AND_BOTTOM = 3  #3D top & bottom stereo mode
-    dsVIDEO_SSMODE_MAX = 4                #Out of range
-
-class hdmiVideoFrameRate(Enum):
-    dsVIDEO_FRAMERATE_UNKNOWN = 0  #Unknown frame rate
-    dsVIDEO_FRAMERATE_24 = 1       # Played at 24 frames per second
-    dsVIDEO_FRAMERATE_25 = 2       # Played at 25 frames per second
-    dsVIDEO_FRAMERATE_30 = 3       # Played at 30 frames per second
-    dsVIDEO_FRAMERATE_60 = 4       # Played at 60 frames per second
-    dsVIDEO_FRAMERATE_23dot98 = 5  # Played at 23.98 frames per second
-    dsVIDEO_FRAMERATE_29dot97 = 6  # Played at 29.97 frames per second
-    dsVIDEO_FRAMERATE_50 = 7       # Played at 50 frames per second
-    dsVIDEO_FRAMERATE_59dot94 = 8  # Played at 59.94 frames per second
-    dsVIDEO_FRAMERATE_MAX = 9      # Out of range
-
 class dsHdmiIn_test4_VideoModeChangeCallback_Verify(utHelperClass):
 
     testName  = "test4_VideoModeChangeCallback_Verify"
@@ -98,18 +64,33 @@ class dsHdmiIn_test4_VideoModeChangeCallback_Verify(utHelperClass):
 
     def testDownloadAssets(self):
         """
-        Downloads the artifacts and streams listed in test-setup configuration file to the dut.
+        Downloads the test artifacts and streams listed in the test setup configuration.
+
+        This function retrieves audio streams and other necessary files and
+        saves them on the DUT (Device Under Test).
 
         Args:
-            None.
+            None
         """
+
+        # List of streams with path
+        self.testStreams = []
 
         self.deviceDownloadPath = self.cpe.get("target_directory")
 
-        #download test artifacts to device
-        url = self.testSetup.assets.device.test4_VideoModeChangeCallback_Verify.artifacts
+        test = self.testSetup.get("assets").get("device").get(self.testName)
+
+        # Download test artifacts to device
+        url = test.get("artifacts")
         if url is not None:
             self.downloadToDevice(url, self.deviceDownloadPath, self.rackDevice)
+
+        # Download test streams to device
+        url =  test.get("streams")
+        if url is not None:
+            self.downloadToDevice(url, self.deviceDownloadPath, self.rackDevice)
+            for streampath in url:
+                self.testStreams.append(os.path.join(self.deviceDownloadPath, os.path.basename(streampath)))
 
     def testCleanAssets(self):
         """
@@ -122,64 +103,66 @@ class dsHdmiIn_test4_VideoModeChangeCallback_Verify(utHelperClass):
 
     def testRunPrerequisites(self):
         """
-        Runs Prerequisite commands listed in test-setup configuration file on the dut.
+        Executes prerequisite commands listed in the test setup configuration file on the DUT.
+
+        Args:
+            None
+        """
+
+        # Run commands as part of test prerequisites
+        test = self.testSetup.get("assets").get("device").get(self.testName)
+        cmds = test.get("execute")
+        if cmds is not None:
+            for cmd in cmds:
+                self.writeCommands(cmd)
+
+    def testRunPostreiquisites(self):
+        """
+        Executes postrequisite commands listed in test-setup configuration file on the DUT.
 
         Args:
             None.
         """
 
-        #Run test specific commands
-        cmds = self.testSetup.assets.device.test4_VideoModeChangeCallback_Verify.execute
+       # Run commands as part of test prerequisites
+        test = self.testSetup.get("assets").get("device").get(self.testName)
+        cmds = test.get("postcmd")
         if cmds is not None:
             for cmd in cmds:
                 self.writeCommands(cmd)
 
-     #TODO: Current version supports only manual verification.
-    def CheckDeviceStatus(self, manual=False, port_type:str:0):
-        """
-        Verifies whether the particular input selected or not.
-
-        Args:
-            manual (bool, optional): Manual verification (True: manual, False: other verification methods).
-                                     Defaults to other verification methods
-
-        Returns:
-            bool
-        """
-        if manual == True:
-            return self.testUserResponse.getUserYN(f'Is {port_type} Hdmi In device is ON? and change the resolution on source side (Y/N):')
-        else :
-            #TODO: Add automation verification methods
-            return False
-
     #TODO: Current version supports only manual verification.
-    def VerifyInput(self, manual=False):
-        """
+    def CheckDeviceStatusAndResolutionChange(self, manual=False, port_type:str=0, resolution:str=0):
+       """
         Verifies whether the particular input selected or not.
-
+        Ask for resolution change on source device.
         Args:
             manual (bool, optional): Manual verification (True: manual, False: other verification methods).
                                      Defaults to other verification methods
 
         Returns:
             bool
-        """
-        if manual == True:
-            return self.testUserResponse.getUserYN("Is HdmiIn port selected? (Y/N):")
-        else :
+       """
+       if manual == True and resolution == False:
+            return self.testUserResponse.getUserYN(f'Check {port_type} Hdmi In device is ON and press Enter:')
+       elif manual == True and resolution == True:
+            return self.testUserResponse.getUserYN(f'Change the resolution on device connected to {port_type}then press Enter:')
+       else:
             #TODO: Add automation verification methods
             return False
-
-    def find_VideoModeStatus(self, input_str: str, vmode_status: str) -> bool:
-        if vmode_status in input_str:
-            return True
-        return False
+       
 
     def testFunction(self):
-        """This Callback will check for Video Mode change events on HdmiIn ports
+        """
+        The main test function video mode of Hdmi In device.
+
+        This function:
+        - Downloads necessary assets.
+        - Runs prerequisite commands.
+        - Verifies HDMI In video mode update through callbacks.
 
         Returns:
-            bool
+            bool: Final result of the test.
         """
 
         # Download the assets listed in test setup configuration file
@@ -193,7 +176,7 @@ class dsHdmiIn_test4_VideoModeChangeCallback_Verify(utHelperClass):
 
         self.log.testStart("test4_VideoModeChangeCallback_Verify", '1')
 
-        # Initialize the dsAudio module
+        # Initialize the dsHdmiIn module
         self.testdsHdmiIn.initialise(self.testdsHdmiIn.getDeviceType())
 
         audmix = 0      #default value false
@@ -201,54 +184,28 @@ class dsHdmiIn_test4_VideoModeChangeCallback_Verify(utHelperClass):
         topmost = 1     #Always should be true.
    
         # Loop through the supported HdmiIn ports
-        for port,index in self.testdsAudio.getSupportedPorts():
+        for port in self.testdsHdmiIn.getSupportedPorts():
             self.log.stepStart(f'Select {port} Port')
             self.log.step(f'Select {port} Port')
 
             # Check the HdmiIn device is active
-            result = self.CheckDeviceStatus(True,port)
-          
-            if result != False:
-                self.testdsHdmiIn.selectPort(port, index, audmix, videoplane, topmost)
-                result = self.VerifyInput(True)
-                self.log.stepResult(result, f'HdmiIn Select Verification {port} Port')
-                
-                result = self.hal_session.read_until("Result dsHdmiInGetCurrentVideoMode OUT:resolution:(resolution name:[  ], pixelResolution:[  ] ,aspectRatio:")
-                print(result)
-                
-                for pixelresolution in list(hdmiVideoResolution):
-                    if find_VideoModeStatus(result,hdmiVideoResolution(pixelresolution).name):
-                        self.log.stepResult(True,f'hdmi Video Resolution {hdmiVideoResolution(pixelresolution).name} in Callback found')
-                    else:
-                        self.log.stepResult(False,f'hdmi Video Resolution {hdmiVideoResolution(pixelresolution).name} in Callback found')
-                
-                for aspectratio in list(hdmiVideoAspectRatio):
-                    if aspectratio != hdmiVideoAspectRatio.dsVIDEO_ASPECT_RATIO_MAX:
-                        if find_VideoModeStatus(result,hdmiVideoAspectRatio(aspectratio).name):
-                            self.log.stepResult(True,f'hdmi aspect ratio {hdmiVideoAspectRatio(aspectratio).name} in Callback found')
-                        else:
-                            self.log.stepResult(False,f'hdmi aspect ratio {hdmiVideoAspectRatio(aspectratio).name} in Callback found')
-
-                result = self.hal_session.read_until("Result dsHdmiInGetCurrentVideoMode OUT:resolution:(stereoScopicMode:[  ], frameRate:[  ], interlaced:[  ]")
-                print(result)
-
-                for stereoscopic in list(hdmiVideoStereoScopicMode):
-                    if stereoscopic != hdmiVideoStereoScopicMode.dsVIDEO_SSMODE_MAX:
-                        if find_VideoModeStatus(result,hdmiVideoStereoScopicMode(stereoscopic).name):
-                            self.log.stepResult(True,f'hdmi stereo scopic {hdmiVideoStereoScopicMode(stereoscopic).name} in Callback found')
-                        else:
-                            self.log.stepResult(False,f'hdmi aspect ratio {hdmiVideoStereoScopicMode(stereoscopic).name} in Callback found')
-               
-                for framerate in list(hdmiVideoFrameRate):
-                    if framerate != hdmiVideoFrameRate.dsVIDEO_FRAMERATE_MAX:
-                        if find_VideoModeStatus(result,hdmiVideoFrameRate(framerate).name):
-                            self.log.stepResult(True,f'hdmi frame rate {hdmiVideoFrameRate(framerate).name} in Callback found')
-                        else:
-                            self.log.stepResult(False,f'hdmi frame rate {hdmiVideoFrameRate(framerate).name} in Callback found')
-
+            self.CheckDeviceStatusAndResolutionChange(True,port,False)
+            self.testdsHdmiIn.selectPort(port, audmix, videoplane, topmost)
+            self.CheckDeviceStatusAndResolutionChange(True,port,True)
+            
+            videomode = self.testdsHdmiIn.getVideoModeCallbackStatus()
+            if port == videomode[0] and videomode[3] == "true":
+                result = True
+                self.log.stepResult(result,f'hdmi video mode port:{port} pixelresolution:{videomode[1]} aspectratio:{videomode[2]} in Callback found')
+            else:
+                result = False
+                self.log.stepResult(result,f'hdmi video mode port:{port} pixelresolution:{videomode[1]} aspectratio:{videomode[2]} in Callback found')
 
         # Clean the assets downloaded to the device
         self.testCleanAssets()
+
+        #Run postrequisites listed in the test setup configuration file 
+        self.testRunPostreiquisites()
 
         # Terminate dsHdmiIn Module
         self.testdsHdmiIn.terminate()
