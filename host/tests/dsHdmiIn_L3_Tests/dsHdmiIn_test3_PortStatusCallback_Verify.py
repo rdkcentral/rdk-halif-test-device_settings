2625ab55b1624c4b10bfb3463b1582ede854ebe1
ishthiyaqahmed <167058902+ishthiyaqahmed@users.noreply.github.com>
2024-10-17 09:29:07 +0000

    gh #202 HdmiIn L3 python testcases review comments addressal.

--- host/tests/dsHdmiIn_L3_Tests/dsHdmiIn_test3_PortStatusCallback_Verify.py
+++ host/tests/dsHdmiIn_L3_Tests/dsHdmiIn_test3_PortStatusCallback_Verify.py
@@ -31,7 +31,6 @@ sys.path.append(os.path.join(dir_path, "../"))
 from dsClasses.dsHdmiIn import dsHdmiInClass
 from raft.framework.plugins.ut_raft import utHelperClass
 from raft.framework.plugins.ut_raft.configRead import ConfigRead
-from raft.framework.plugins.ut_raft.utPlayer import utPlayer
 from raft.framework.plugins.ut_raft.utUserResponse import utUserResponse
 
 class dsHdmiIn_test3_PortStatusCallback_Verify(utHelperClass):
@@ -64,18 +63,33 @@ class dsHdmiIn_test3_PortStatusCallback_Verify(utHelperClass):
 
     def testDownloadAssets(self):
         """
-        Downloads the artifacts and streams listed in test-setup configuration file to the dut.
+        Downloads the test artifacts and streams listed in the test setup configuration.
+
+        This function retrieves audio streams and other necessary files and
+        saves them on the DUT (Device Under Test).
 
         Args:
-            None.
+            None
         """
+
+        # List of streams with path
+        self.testStreams = []
 
         self.deviceDownloadPath = self.cpe.get("target_directory")
 
-        #download test artifacts to device
-        url = self.testSetup.assets.device.test3_PortStatusCallback_Verify.artifacts
+        test = self.testSetup.get("assets").get("device").get(self.testName)
+
+        # Download test artifacts to device
+        url = test.get("artifacts")
         if url is not None:
             self.downloadToDevice(url, self.deviceDownloadPath, self.rackDevice)
+
+        # Download test streams to device
+        url =  test.get("streams")
+        if url is not None:
+            self.downloadToDevice(url, self.deviceDownloadPath, self.rackDevice)
+            for streampath in url:
+                self.testStreams.append(os.path.join(self.deviceDownloadPath, os.path.basename(streampath)))
 
     def testCleanAssets(self):
         """
@@ -88,27 +102,28 @@ class dsHdmiIn_test3_PortStatusCallback_Verify(utHelperClass):
 
     def testRunPrerequisites(self):
         """
-        Runs Prerequisite commands listed in test-setup configuration file on the dut.
+        Executes prerequisite commands listed in the test setup configuration file on the DUT.
 
         Args:
-            None.
+            None
         """
 
-        #Run test specific commands
-        cmds = self.testSetup.assets.device.test3_PortStatusCallback_Verify.execute
+        # Run commands as part of test prerequisites
+        test = self.testSetup.get("assets").get("device").get(self.testName)
+        cmds = test.get("execute")
         if cmds is not None:
             for cmd in cmds:
                 self.writeCommands(cmd)
 
      #TODO: Current version supports only manual verification.
-    def CheckDeviceStatus(self, manual=False, port_type:str:0):
+    def CheckDeviceStatus(self, manual=False, port_type:str=0):
         """
-        Verifies whether the particular input selected or not.
+        Checks Device Power status is ON.
 
         Args:
+            port (str) : HDMI port
             manual (bool, optional): Manual verification (True: manual, False: other verification methods).
                                      Defaults to other verification methods
-
         Returns:
             bool
         """
@@ -119,15 +134,8 @@ class dsHdmiIn_test3_PortStatusCallback_Verify(utHelperClass):
             return False
     
     def testRunPostreiquisites(self):
-            return self.testUserResponse.getUserYN("Is {port_type} Hdmi In device is ON? (Y/N):")
-        else :
-            #TODO: Add automation verification methods
-            return False
-
-    #TODO: Current version supports only manual verification.
-    def VerifyInput(self, manual=False):
         """
-        Verifies whether the particular input selected or not.
+        Executes postrequisite commands listed in the test setup configuration file on the DUT.
 
         Args:
             None
@@ -140,17 +148,17 @@ class dsHdmiIn_test3_PortStatusCallback_Verify(utHelperClass):
             for cmd in cmds:
                 self.writeCommands(cmd)
 
-
-    def find_PortStatus(self, input_str: str, port_status: str) -> bool:
-        if port_status in input_str:
-            return True
-        return False
-
     def testFunction(self):
-        """This Callback will Port Status Change events on HdmiIn ports
+        """
+        The main test function that verifies signal status of Hdmi In device.
+
+        This function:
+        - Downloads necessary assets.
+        - Runs prerequisite commands.
+        - Verifies port status through callbacks.
 
         Returns:
-            bool
+            bool: Final result of the test.
         """
 
         # Download the assets listed in test setup configuration file
@@ -164,7 +172,7 @@ class dsHdmiIn_test3_PortStatusCallback_Verify(utHelperClass):
 
         self.log.testStart("test3_PortStatusCallback_Verify", '1')
 
-        # Initialize the dsAudio module
+        # Initialize the dsHdmiIn module
         self.testdsHdmiIn.initialise(self.testdsHdmiIn.getDeviceType())
 
         audmix = 0      #default value false
@@ -172,7 +180,7 @@ class dsHdmiIn_test3_PortStatusCallback_Verify(utHelperClass):
         topmost = 1     #Always should be true.
    
         # Loop through the supported HdmiIn ports
-        for port,index in self.testdsAudio.getSupportedPorts():
+        for port in self.testdsHdmiIn.getSupportedPorts():
             self.log.stepStart(f'Select {port} Port')
             self.log.step(f'Select {port} Port')
 
@@ -191,25 +199,11 @@ class dsHdmiIn_test3_PortStatusCallback_Verify(utHelperClass):
                result = False
                self.log.stepResult(result,f'Port Status ispresented:{status[0]} activeport:{status[1]} found in Callback')
 
-            # Check the HdmiIn device is active
-            result = self.CheckDeviceStatus(True,port)
-          
-            if result != False:
-                self.testdsHdmiIn.selectPort(port, index, audmix, videoplane, topmost)
-                result = self.VerifyInput(True)
-                self.log.stepResult(result, f'HdmiIn Select Verification {port} Port')
-                
-                result = self.hal_session.read_until("Received statuschange callback isPresented:")
-                print(result)
-                
-                if find_PortStatus(result,"true"):
-                    self.log.stepResult(True,f'Port Status ispresented:true and  activeport:{port} found in Callback found')
-                else:
-                    self.log.stepResult(False,f'Port Status ispresented:false found in Callback found')
-
-
         # Clean the assets downloaded to the device
         self.testCleanAssets()
+  
+        #Run postrequisites listed in the test setup configuration file 
+        self.testRunPostreiquisites()
 
         # Terminate dsHdmiIn Module
         self.testdsHdmiIn.terminate()
